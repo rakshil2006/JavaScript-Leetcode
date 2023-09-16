7599bc325751e915132fc8d93819518556e71ecc
Rakshil Gajjar <142925277+rakshil2006@users.noreply.github.com>
2023-09-16 23:56:29 +0530

    day2.js

--- day2.js
+++ day2.js
@@ -1,12 +1,16 @@
-function createCounter(n) {
-  return function() {
-    return n++;
-  };
-}
+**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
 
-// Example usage:
-const myCounter = createCounter(5);
-
-console.log(myCounter()); // Output: 5
-console.log(myCounter()); // Output: 6
-console.log(myCounter()); // Output: 7
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
