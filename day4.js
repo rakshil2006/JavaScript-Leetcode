b231fc175d8fec12c678d56ec613072cd2132e91
Rakshil Gajjar <142925277+rakshil2006@users.noreply.github.com>
2023-10-30 16:18:54 +0530

    Update day4.js

--- day4.js
+++ day4.js
@@ -1,7 +1,7 @@
 /**
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
- */
+ */ 
 var createCounter = function(init) {
     var i = init;
     return {
