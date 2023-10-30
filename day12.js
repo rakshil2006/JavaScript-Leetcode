82f42070d78cf828b763f886d29361d5fcc23122
Rakshil Gajjar <142925277+rakshil2006@users.noreply.github.com>
2023-10-30 16:07:46 +0530

    Create day12.js

---
+++ day12.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    try {
+        const [res1, res2] = await Promise.all([promise1, promise2]);
+        const sum = res1 + res2;
+        return Promise.resolve(sum);
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
